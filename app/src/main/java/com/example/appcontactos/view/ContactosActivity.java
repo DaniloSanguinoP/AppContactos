285a4852221af520e7d6051c52dece763a747947
DaniloAndresSP <76256087+DaniloAndresSP@users.noreply.github.com>
2023-02-12 23:15:50 -0500

    IMPLEMENTACIÓN - Splash screen
    * Se crea el diseño Splash screen
    * Se crean animaciones para la transición entre actividades.
    * Se añade la librería de lottie para el uso de animaciones.

---
+++ app/src/main/java/com/example/appcontactos/view/ContactosActivity.java
@@ -0,0 +1,16 @@
+package com.example.appcontactos.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appcontactos.R;
+
+public class ContactosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactos);
+    }
+}
--- app/src/main/java/com/example/appcontactos/view/MainActivity.java
+++ app/src/main/java/com/example/appcontactos/view/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.appcontactos.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.appcontactos.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, ContactosActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,3000);
     }
 }
