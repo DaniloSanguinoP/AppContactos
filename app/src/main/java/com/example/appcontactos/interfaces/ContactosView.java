2b22cf9f8f33c90fca2014edd30f9d23a4c3f87c
DaniloAndresSP <76256087+DaniloAndresSP@users.noreply.github.com>
2023-02-12 22:41:42 -0500

    IMPLEMENTACION - Patrón de diseño MVP
    * Se crea la estructura básica del modelo MVP.
    * Se crean las Interfaces. 

---
+++ app/src/main/java/com/example/appcontactos/interfaces/ContactosView.java
@@ -0,0 +1,4 @@
+package com.example.appcontactos.interfaces;
+
+public interface ContactosView {
+}
--- app/src/main/java/com/example/appcontactos/view/MainActivity.java
+++ app/src/main/java/com/example/appcontactos/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.appcontactos;
+package com.example.appcontactos.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.appcontactos.R;
 
 public class MainActivity extends AppCompatActivity {
 
